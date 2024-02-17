3697cd7615d264007e4f5bf5b3a9b076f037477f
Joshua Uzzell <josh.uzzell@gmail.com>
2024-02-17 08:39:31 -0500

    patch/usage-notes, Adds usage notes to app.php on how to use command line

--- src/app.php
+++ src/app.php
@@ -1,1 +1,20 @@
-<? // See Readme.md or use php console.php 
+<?
+    // See Readme.md or use php console.php
+    
+    /**
+     * Sequence of console commands
+     * 
+     * 1. php console.php RunStatementsReports
+     * 2. php console.php PostProcessReports
+     * 3. php console.php Migrate
+     * 4. php console.php ImportCsvToDatabase
+     * 5. php console.php GenerateStats
+     */
+/*
+rm tmp/data/analytics-database.db &&
+php console.php RunStatementsReports &&
+php console.php PostProcessReports &&
+php console.php Migrate &&
+php console.php ImportCsvToDatabase &&
+php console.php GenerateStats
+*/
